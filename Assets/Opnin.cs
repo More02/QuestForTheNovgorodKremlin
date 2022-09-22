6b03f308004751311660a1bb5091c7bd6b093bd4
Marina <mbakanova2016@yandex.ru>
2022-09-22 12:16:12 +0300

    version from presentation

---
+++ Assets/Opnin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opnin : MonoBehaviour {
+
+	public GameObject buttonToClose1;
+	public GameObject buttonToClose2;
+	public GameObject buttonToClose3;
+	public GameObject buttonToClose4;
+	public GameObject buttonWrong;
+	public GameObject model;
+	//public GameObject buttonRight;
+
+
+	void Start () {
+
+	}
+
+	void OnMouseDown() {
+		Open ();
+	}
+
+	public void Open(){
+		buttonToClose1.SetActive (false);
+		buttonToClose2.SetActive (false);
+		buttonToClose3.SetActive (false);
+		buttonToClose4.SetActive (false);
+		buttonWrong.SetActive (true);
+		model.SetActive (false);
+		//buttonRight.SetActive (true);
+	}
+}
